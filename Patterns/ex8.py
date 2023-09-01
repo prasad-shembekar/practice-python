1fb5c736e9b3d21d7768eabe74f4fa7eb4d6afcf
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-09-01 16:31:51 +0530

    Update

--- Patterns/ex8.py
+++ Patterns/ex8.py
@@ -1,6 +1,6 @@
 for r in range(7):
     for c in range(5):
-        if c==0 or (c==4 and (r!=0 and r!=3)) or (r==0 or r==3 and(c>0 and c<4)):
+        if ((c==0 or c==4) and r!=0) or ((r==0 or r==3)and(c>0 and c<4)):
             print("*",end="")
         else:
             print(end=" ")
