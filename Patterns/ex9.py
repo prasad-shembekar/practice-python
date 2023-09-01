4072b24fb7e46b6623ec681cf18370adceb8819e
Prasad Shembekar <prasadmshembekar@gmail.com>
2023-09-01 16:37:24 +0530

    Update

---
+++ Patterns/ex9.py
@@ -0,0 +1,7 @@
+for r in range(7):
+    for c in range(5):
+        if((r==0 or r==3 or r==6) and (c>0 and c<4)) or (c==0 and(r>0 and r<3)) or (c==4 and(r>3 and r<6)):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print(end="\n")
